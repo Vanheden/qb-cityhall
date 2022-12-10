b2f4e61c9b02af8567103b3adc7235f9eb445fd1
Vanheden <smultronstigen12@gmail.com>
2022-12-10 20:15:26 +0100

    Create sv.lua

---
+++ locales/sv.lua
@@ -0,0 +1,28 @@
+local Translations = {
+    error = {
+        not_in_range = 'För långt ifrån Stadshuset'
+    },
+    success = {
+        recived_license = 'Du har fått ditt %{value} för $50'
+    },
+    info = {
+        bilp_text = 'Stadstjänster',
+        city_services_menu = '~g~E~w~ - Stadstjänst Menu',
+        id_card = 'ID Kort',
+        driver_license = 'Körkort',
+        weaponlicense = 'Vapen Licens',
+        new_job = 'Grattis till ditt nya! (%{job})'
+    },
+    email = {
+        mr = 'Mr',
+        mrs = 'Mrs',
+        sender = 'Kommun',
+        subject = 'Begär Körlektioner',
+        message = 'Hej %{gender} %{efternamn}<br /><br />Vi har precis fått ett meddelande om att någon vill ta körlektioner<br />Om du är villig att undervisa, vänligen kontakta oss:<br />Namn : <strong>%{firstname} %{efternamn}</strong><br />Telefonnummer: <strong>%{phone}</strong><br/><br/>Vänliga hälsningar,<br />Township Los Santos'
+    }
+}
+
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
